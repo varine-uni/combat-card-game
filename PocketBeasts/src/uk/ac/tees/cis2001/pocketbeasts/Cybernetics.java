cb88e3840f0deb2864e50d110afba8bf4cc18319
Ciaran May-Townsend <maytownsendciaran@gmail.com>
2021-01-08 16:01:06 +0000

    Changed purpose of Card class. Implemented the first class: Imp card.

--- PocketBeasts/src/uk/ac/tees/cis2001/pocketbeasts/Cybernetics.java
+++ PocketBeasts/src/uk/ac/tees/cis2001/pocketbeasts/Cybernetics.java
@@ -1,7 +1,18 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * This file is part of PocketBeasts.
+ *
+ * PocketBeasts is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PocketBeasts is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
  */
 package uk.ac.tees.cis2001.pocketbeasts;
 
@@ -9,13 +20,21 @@ package uk.ac.tees.cis2001.pocketbeasts;
  *
  * @author w9101532
  */
-public class Cybernetics
+public class Cybernetics extends Card 
 {
+
+    public Cybernetics()
+    {
+        
+    }
+    
     enum BodyPart //Cybernetic objects can either be an arm or leg cybernetic.
     {
         LEGS,
         ARMS
     }
     
+    int c_Damage; //Cybernetic damage, if there is any.
+    BodyPart c_BodyPart; //Body part selection.
     
 }
