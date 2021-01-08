a3d1e4d724d28d787d215fa1f6127c1ea881ecde
Ciaran May-Townsend <maytownsendciaran@gmail.com>
2021-01-08 02:58:08 +0000

    Test commit

---
+++ PocketBeasts/src/uk/ac/tees/cis2001/pocketbeasts/Cybernetics.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uk.ac.tees.cis2001.pocketbeasts;
+
+/**
+ *
+ * @author w9101532
+ */
+public class Cybernetics
+{
+    enum BodyPart //Cybernetic objects can either be an arm or leg cybernetic.
+    {
+        LEGS,
+        ARMS
+    }
+    
+    
+}
